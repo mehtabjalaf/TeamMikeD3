aec3125dc29a347ee8c65970d0eed927fed2d500
jalaf-mehtab <msj420@my.yorku.ca>
2023-04-14 20:50:47 -0400

    Metric Info
    This is the folder for CodeMR in our project, feel free to explore it and check our HTML graphs for the metrics.

---
+++ codemr/3311 Deliverable/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"3311 Deliverable", "Number of Highly Problematic Classes":0, "Number of Entities":14, "Number of Problematic Classes":0, "Class Lines of Code":921, "Number of External Packages":35, "Number of Packages":1, "Number of External Entities":191}
---
+++ codemr/3311 Deliverable/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: '3311 Deliverable', path:'3311-deliverable'}
+];
